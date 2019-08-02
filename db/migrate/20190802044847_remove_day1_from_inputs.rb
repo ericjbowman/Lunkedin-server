76c988a7dd8bcb8188d0365b645955f8402f8233
ericbowman <ericjbowman@gmail.com>
2019-08-02 01:05:31 -0400

    Remove day1

--- db/migrate/20190802044847_remove_day1_from_inputs.rb
+++
@@ -1,4 +0,0 @@
-class RemoveDay1FromInputs < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/migrate/20190802045517_remove_day1_from_inputs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDay1FromInputs < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :inputs, :day1, :boolean
-  end
-end
