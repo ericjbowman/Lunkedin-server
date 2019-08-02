f7210d17d8cba1fe7e0ac6807d5a0e99bd955561
ericbowman <ericjbowman@gmail.com>
2019-08-02 00:52:34 -0400

    AddDay1ToInputs will not go away!

--- db/migrate/20190801224819_add_day1_to_inputs.rb
+++
@@ -1,5 +0,0 @@
-class AddDay1ToInputs < ActiveRecord::Migration[5.2]
-  def change
-    add_column :inputs, :day1, :boolean
-  end
-end
---
+++ db/migrate/20190802044847_remove_day1_from_inputs.rb
@@ -0,0 +1,4 @@
+class RemoveDay1FromInputs < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
